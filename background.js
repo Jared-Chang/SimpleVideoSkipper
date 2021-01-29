d32041e00a1f728f38bb8ad134090d5762d1d935
QiFen.Pocket <QiFen.Pocket@gmail.com>
2021-01-29 16:25:45 +0800

    fix: cannot use

--- background.js
+++ background.js
@@ -1,24 +1,23 @@
 chrome.commands.onCommand.addListener(function(command) {
-  
-  let video = document.getElementsByTagName("video")[0];
-
-  if (!video) {
-    return
-  }
+  let script = 'document.getElementsByTagName("video")[0].currentTime';
 
   switch (command)
   {
     case "skip-forward-5":
-      video.currentTime -= 5;
+      script += ' += 5';
       break;
     case "skip-5":
-      video.currentTime += 5;
+      script += ' -= 5';
       break;
     case "skip-forward-10":
-      video.currentTime -= 10;
+      script += ' += 10';
       break;
     case "skip-10":
-      video.currentTime += 10;
+      script += ' -= 10';
       break;
   }
+
+  chrome.tabs.executeScript({
+    code: script
+  });
 });
